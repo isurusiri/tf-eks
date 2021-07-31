f9f17fefef56192724511ceec35d1109fbaeeab7
Isuru Siriwardana <isuruanatomy@gmail.com>
2021-07-31 10:35:34 +0530

    security group module impl

---
+++ modules/securitygroups/main.tf
@@ -0,0 +1,34 @@
+resource "aws_security_group" "node-sg" {
+  name        = var.name
+  description = var.description
+  vpc_id      = var.vpc_id
+
+  egress {
+    from_port   = var.egress_from_port
+    to_port     = var.egress_to_port
+    protocol    = var.egress_protocol
+    cidr_blocks = var.egress_cidr_blocks
+  }
+
+  tags = var.tags
+}
+
+resource "aws_security_group_rule" "node-ingress-self" {
+  description              = var.node_ingress_description
+  from_port                = var.node_ingress_from_port
+  protocol                 = var.node_ingress_protocol
+  security_group_id        = aws_security_group.node-sg.id
+  source_security_group_id = !var.is_worker ? var.node_ingress_source_sg_id : aws_security_group.node-sg.id
+  to_port                  = var.node_ingress_to_port
+  type                     = "ingress"
+}
+
+resource "aws_security_group_rule" "node-ingress-cluster" {
+  description              = var.cluster_ingress_description
+  from_port                = var.cluster_ingress_from_port
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.node-sg.id
+  source_security_group_id = var.is_worker ? aws_security_group.dev-cluster.id : null
+  to_port                  = var.cluster_ingress_to_port
+  type                     = "ingress"
+}
--- modules/securitygroups/variables.tf
+++ modules/securitygroups/variables.tf
@@ -1,1 +1,81 @@
 
+variable "name" {
+  type = string
+}
+
+variable "description" {
+  default = "Cluster security group"
+  type    = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+
+variable "egress_from_port" {
+  type = number
+}
+
+variable "egress_to_port" {
+  type = number
+}
+
+variable "egress_protocol" {
+  default = "-1"
+  type    = string
+}
+
+variable "egress_cidr_blocks" {
+  default = ["0.0.0.0/0"]
+  type    = list(string)
+}
+
+variable "tags" {
+  type = map(string)
+}
+
+variable "node_ingress_description" {
+  type = string
+}
+
+variable "node_ingress_from_port" {
+  default = 0
+  type    = number
+}
+
+variable "node_ingress_protocol" {
+  default = "-1"
+  type    = string
+}
+
+variable "node_ingress_to_port" {
+  default = 65535
+  type    = number
+}
+
+variable "cluster_ingress_description" {
+  type = string
+}
+
+variable "cluster_ingress_from_port" {
+  default = 1025
+  type    = number
+}
+
+variable "cluster_ingress_source_sg_id" {
+  type = string
+}
+
+variable "node_ingress_source_sg_id" {
+  type = string
+}
+
+variable "cluster_ingress_to_port" {
+  default = 65535
+  type    = number
+}
+
+variable "is_worker" {
+  type = bool
+}
