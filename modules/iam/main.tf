d5b43509ecf2fbcb0f0c4c78bca90a7f7eda5646
Isuru Siriwardana <isuruanatomy@gmail.com>
2021-08-08 19:51:09 +0530

    added iam module

---
+++ modules/iam/main.tf
@@ -0,0 +1,56 @@
+resource "aws_iam_role" "dev-cluster" {
+  name = "terraform-eks-dev-cluster"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+
+}
+
+resource "aws_iam_role_policy_attachment" "dev-cluster-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.dev-cluster.name
+}
+
+resource "aws_iam_role_policy_attachment" "dev-cluster-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.dev-cluster.name
+}
+
+resource "aws_iam_role_policy" "dev-cluster-service-linked-role" {
+  name = "service-linked-role"
+  role = aws_iam_role.dev-cluster.name
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "iam:CreateServiceLinkedRole",
+            "Resource": "arn:aws:iam::*:role/aws-service-role/*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DescribeAccountAttributes"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+
+}
+
