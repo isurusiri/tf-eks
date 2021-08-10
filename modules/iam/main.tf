49bfb7cfa6cceab6ea30c46cfa225e80d2f8b16f
Isuru Siriwardana <isuruanatomy@gmail.com>
2021-08-10 11:12:17 +0530

    iam module implementation

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,7 +1,5 @@
-resource "aws_iam_role" "dev-cluster" {
-  name = "terraform-eks-dev-cluster"
-
-  assume_role_policy = <<POLICY
+locals {
+  server_assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -16,21 +14,44 @@ resource "aws_iam_role" "dev-cluster" {
 }
 POLICY
 
+  node_assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
 }
 
-resource "aws_iam_role_policy_attachment" "dev-cluster-AmazonEKSClusterPolicy" {
+resource "aws_iam_role" "iam_role" {
+  name               = var.iam_role_name
+  assume_role_policy = var.is_worker ? local.node_assume_role_policy : local.server_assume_role_policy
+
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+  count      = var.is_worker ? 0 : 1
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.dev-cluster.name
+  role       = aws_iam_role.iam_role.name
 }
 
-resource "aws_iam_role_policy_attachment" "dev-cluster-AmazonEKSServicePolicy" {
+resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
+  count      = var.is_worker ? 0 : 1
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = aws_iam_role.dev-cluster.name
+  role       = aws_iam_role.iam_role.name
 }
 
-resource "aws_iam_role_policy" "dev-cluster-service-linked-role" {
-  name = "service-linked-role"
-  role = aws_iam_role.dev-cluster.name
+resource "aws_iam_role_policy" "service-linked-role" {
+  count = var.is_worker ? 0 : 1
+  name  = "service-linked-role"
+  role  = aws_iam_role.iam_role.name
 
   policy = <<EOF
 {
@@ -54,3 +75,26 @@ EOF
 
 }
 
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  count      = var.is_worker ? 1 : 0
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.iam_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  count      = var.is_worker ? 1 : 0
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.iam_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  count      = var.is_worker ? 1 : 0
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.iam_role.name
+}
+
+resource "aws_iam_instance_profile" "iam_instance_profile" {
+  count = var.is_worker ? 1 : 0
+  name  = "terraform-eks-dev"
+  role  = aws_iam_role.iam_role.name
+}
